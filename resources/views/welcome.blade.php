dbeaa99401c3b2751f6daff64451eadebc96e819
Jacky Choo <jacky.choo@checknow.org>
2019-02-02 02:49:21 +0800

    change title again zz

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Airliances again
+                    Airliancess
                 </div>
 
                 <div class="links">
