507eb6ec7f4d2c0c6cc2c97e94f11e93b38f7f1e
Jacky Choo <jacky.choo@checknow.org>
2019-02-04 02:02:55 +0800

    added seed for area

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Area;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +11,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('areas')->delete();
+
+        $areas = [ 
+            new Area(['id' => 4723, 'name' => 'Kuala Lumpur', 'countrycode' => 'MY' ]),
+            new Area(['id' => 16654, 'name' => 'Taiping', 'countrycode' => 'MY' ]),
+            new Area(['id' => 19799, 'name' => 'Penang', 'countrycode' => 'MY' ]),
+            new Area(['id' => 23340, 'name' => 'Ipoh', 'countrycode' => 'MY' ])
+        ];
+
+        foreach ($areas as $area) {
+            $area->save();
+        }
     }
 }
