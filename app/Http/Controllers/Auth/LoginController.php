23581f108de7127837544ef4e7e8dfd76d25049e
Jacky Choo <jacky.choo@checknow.org>
2019-02-04 04:08:34 +0800

    add redirect after login to booking screen

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/bookings/new';
 
     /**
      * Create a new controller instance.
